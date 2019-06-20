e8d0c9625aa5647944475b62ba0398ecc7092399
Artem <43318829+ArtemPrydorozhko@users.noreply.github.com>
2019-06-20 16:58:33 +0300

    parser

--- index.js
+++ index.js
@@ -1,29 +1,88 @@
 const http = require('http');
-const url = require('url');
-const net = require('net');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
-const server = http.createServer((request, response) => {
-    const req = http.request(request.url,{method: 'GET'},(res)=>{
-        response.writeHead(200,res.headers);
-        response.pipe(res);
-    }).on('error', err=>{
-        console.log("Error:"+err.message);
+const dataUrl = 'http://rozklad.kpi.ua/Schedules/ViewSchedule.aspx?g=b41a3ace-d3f0-4b64-9d15-2b7f9a970aa6';
+var rawData = '';
+
+http.get(dataUrl, (res) =>{
+    res.setEncoding('utf8');
+    res.on('data', (chunk)=>{
+        rawData+=chunk;
     });
-    req.end();
+    res.on('end', ()=>{
+        parse(rawData);
+    });
+}).on('error', (err)=>{
+    console.log('Error', err.message);
 });
 
-server.on('connect', (req, socket) => {
-    const srvUrl = url.parse(`http://${req.url}`);
-    const srvSocket = net.connect(srvUrl.port, srvUrl.hostname, () => {
-        socket.write(
-            `HTTP/${req.httpVersion} 200 OK\r\nConnection: close\r\n\r\n`,
-            'UTF-8',
-            () => {
-                srvSocket.pipe(socket);
-                socket.pipe(srvSocket);
+function parse(data) {
+    const dom = new JSDOM(data);
+
+    const firstWeekElem = dom.window.document.getElementById('ctl00_MainContent_FirstScheduleTable');
+    const secondWeekElem = dom.window.document.getElementById('ctl00_MainContent_SecondScheduleTable');
+
+    const firstWeek = parseWeek(firstWeekElem);
+    const secondtWeek = parseWeek(secondWeekElem);
+
+    return [formatResult(firstWeek,1),formatResult(secondtWeek,2)];
+}
+
+function parseWeek(week) {
+    const rows = week.getElementsByTagName('tr');
+    const resultRows = [];
+    for(let i = 1; i<rows.length; i++) 
+    {
+        const tds = rows[i].getElementsByTagName('td');
+        let resultRow = [];
+        for(let j = 1; j<tds.length; j++) 
+        {
+            let lesson = {};
+            lesson.number = i;
+            if(tds[j].innerHTML == '')
+            {
+                resultRow.push(lesson);
+                continue;
+            } else {
+                const lessonDetails = tds[j].getElementsByTagName('a');
+
+                lesson.subj = lessonDetails[0].innerHTML;
+                if(lessonDetails[1] != undefined)
+                {
+                    lesson.teacher = lessonDetails[1].innerHTML;
+                    lesson.loc = lessonDetails[2].innerHTML;
+                }
+
+                resultRow.push(lesson);
             }
-        )
-    })
-});
+        }
+        resultRows.push(resultRow);
+    }
+    return resultRows;
+}
 
-server.listen(8080);
+function formatResult(data, numWeek) {
+    let result = '';
+    const weekDays = ['Понеділок',"Вівторок","Середа","Четвер","П'ятниця","Субота"];
+    result += '===Week '+numWeek+'===/n';
+    for(let i = 0; i < data[0].length; i++)
+    {
+        result += '--' + weekDays[i] + '--/n';
+        for(let j =0; j < data.length; j++)
+        {
+            if(data[j][i].subj == undefined)
+            {
+                result += data[j][i].number + './n'
+            } else {
+                result += data[j][i].number + data[j][i].subj + '/n';
+
+                if(data[j][i].teacher != undefined)
+                {
+                    result += data[j][i].teacher + '/n' + data[j][i].loc +'/n';
+                }
+            }
+        }
+    }
+    return result;
+}
