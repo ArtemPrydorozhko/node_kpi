d68f6a71fbae0befda8e963ef21e4a358d28f9cf
Artem <43318829+ArtemPrydorozhko@users.noreply.github.com>
2019-06-20 23:46:18 +0300

    fixed

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const getSchedule = require('./parse');
 
 const bot = new Telegraf('803465271:AAGpYdbzntcUgNcz_D9puE46gEUBZsxrFTs');
 
-bot.start((ctx) => ctx.reply('Hello! enter /get'));
+bot.start((ctx) => ctx.reply('Hello! enter "get"'));
 bot.hears( 'get',(ctx) => getSchedule(ctx.message.text).then(result => {
         ctx.reply(result);
-    })
-    .catch(() => ctx.reply('Error occured')));
+}).catch(() => ctx.reply('Error occured')));
 
-bot.telegram.setWebhook('https://node-kpi-bot-git-schedule.artemap85.now.sh');
+bot.startPolling();
+bot.telegram.setWebhook('https://node-kpi-bot.artemap85.now.sh');
 
 module.exports = bot.webhookCallback('/');
